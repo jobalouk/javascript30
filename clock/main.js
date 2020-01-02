f6d7df5fe68d46bbdc10c339b65dc05a93eed2c2
Joackim Balouka <jobalouka@gmail.com>
2020-01-02 21:45:48 +0100

    finishing clock

--- clock/main.js
+++ clock/main.js
@@ -1,26 +1,46 @@
-const second = document.getElementsByClassName('second');
-
-const today = new Date();
-
-console.log(today.getSeconds());
-
-for (let i = 1; i < 60; i++ ) {
-  console.log(doSetTimeout(i));
-}
-
-function doSetTimeout(i) {
-  return setTimeout(() => i, 1000)
-}
-
 // get initial hours, mins and seconds
 // 360/60
 // 15*6
+// position = hour * 30
+// hourOffset = 0.5 * minutes
+// position += hourOffset
 
 
-function getInitialSeconds() {
-  return today.getSeconds() * 6 + 'deg'
+
+const today = new Date();
+const secondsNode = document.querySelector('.seconds');
+const minutesNode = document.querySelector('.minutes');
+const hourNode = document.querySelector('.hour');
+
+
+let hours = today.getHours();
+hours = hours % 12;
+hours = hours ? hours : 12;
+
+window.onload = function() {
+  minutesNode.style.webkitTransform = `rotate(${today.getMinutes() * 6}deg)`;
+  secondsNode.style.webkitTransform = `rotate(${today.getSeconds() * 6}deg)`;
+  hourNode.style.webkitTransform = `rotate(${hours * 30}deg)`;
 }
 
-console.log(getInitialSeconds());
+seconds();
 
-second[0].style.webkitTransform = `rotate(${getInitialSeconds()})`
+function seconds() {
+  let x = today.getSeconds() + 1;
+  let y = today.getMinutes();
+
+  setInterval(() => {
+      if (x > 60) {
+        x = 0;
+        y++;
+
+        secondsNode.style.webkitTransform = `rotate(${0}deg)`;
+        minutesNode.style.webkitTransform = `rotate(${y * 6}deg)`
+        x++;
+      }
+
+      secondsNode.style.webkitTransform = `rotate(${x * 6}deg)`;
+
+      x++;
+  }, 1000);
+}
